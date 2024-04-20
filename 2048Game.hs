9e25ae6688377477cc87b428b8305fc3b8f02c07
ruanderson <ruanderson.gabriel@academico.ufpb.br>
2024-04-20 17:10:32 -0300

    aa

--- 2048Game.hs
+++ 2048Game.hs
@@ -7,23 +7,39 @@ data Direction = Up_side | Left_side | Down_side | Right_side
 -- Definição do tabuleiro
 type Board = [[Int]]
 
-slide_left :: Board -> Board
-slide_left = map slide_line
-    where   slide_line [] = [] -- linha vazia
-            slide_line [x] = [x] -- apenas um valor
-            slide_line (x: y: xs)  -- dois ou mais valores
-                | x == 0 = slide_line (y : xs) ++ [0] -- Se o primeiro elemento for zero, desliza-se a lista removendo o zero e adicionando-o ao final.
-                | y == 0 = slide_line (x : xs) ++ [0] -- Se o segundo elemento for zero, desliza-se a lista removendo o zero e adicionando-o ao final, permitindo que os elementos se combinem.
-                | x == y = (x + y) : slide_line xs ++ [0] -- Se os dois primeiros elementos forem iguais, combina-se esses dois elementos somando-os e adicionando o resultado ao início da lista resultante.
-                | otherwise = x : slide_line (y : xs) -- Se nenhum dos casos anteriores for verdadeiro, mantém-se o primeiro elemento e continua-se a deslizar a lista.
+slideLeft :: Board -> Board
+slideLeft = map slideLine
+    where   slideLine [] = [] -- linha vazia
+            slideLine [x] = [x] -- apenas um valor
+            slideLine (x: y: xs)  -- dois ou mais valores
+                | x == 0 = slideLine (y : xs) ++ [0] -- Se o primeiro elemento for zero, desliza-se a lista removendo o zero e adicionando-o ao final.
+                | y == 0 = slideLine (x : xs) ++ [0] -- Se o segundo elemento for zero, desliza-se a lista removendo o zero e adicionando-o ao final, permitindo que os elementos se combinem.
+                | x == y = (x + y) : slideLine xs ++ [0] -- Se os dois primeiros elementos forem iguais, combina-se esses dois elementos somando-os e adicionando o resultado ao início da lista resultante.
+                | otherwise = x : slideLine (y : xs) -- Se nenhum dos casos anteriores for verdadeiro, mantém-se o primeiro elemento e continua-se a deslizar a lista.
 
--- desliza os elementos
+-- Desliza os elementos
 slide :: Direction -> Board -> Board
-slide Up_side = transpose.slide_left.transpose
-slide Left_side = map reverse.slide_left.map reverse
-slide Down_side = transpose.map reverse.slide_left.map reverse.transpose
-slide Right_side  = slide_left
+slide Up_side = transpose.slideLeft.transpose
+slide Left_side = map reverse.slideLeft.map reverse
+slide Down_side = transpose.map reverse.slideLeft.map reverse.transpose
+slide Right_side  = slideLeft
 
--- verifica se conseguiu vencer
+-- Verifica se conseguiu vencer
 win :: Board -> Bool
 win x = any (elem 2048) x
+
+
+stucked :: Board -> Bool
+stucked b = all stucked_ b && all stucked_ (transpose b) 
+  where stucked_ row = notElem 0 row && noNeighbors row
+        noNeighbors [_] = True
+        noNeighbors [ ] = True
+        noNeighbors (x:y:xs)
+          | x == y    = False
+          | otherwise = noNeighbors (y:xs)
+
+-- Retorna a posição dos lugares vazios
+emptyPlaces :: Board -> [(Int, Int)]
+emptyPlaces = concatMap (uncurry search).zip [0..3]
+    where search line_index = zip(replicate 4 line_index).elemIndices 0
+
